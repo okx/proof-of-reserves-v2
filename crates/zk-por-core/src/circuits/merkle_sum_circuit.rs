39ee32be84f5179d38a1b94f28a5ca28c2e1af28
EkamSinghPandher <ekamsingh@Ekam-Singhs-MacBook-Pro.local>
2024-07-26 14:33:23 +0800

    Add merkle sum circuits

---
+++ crates/zk-por-core/src/circuits/merkle_sum_circuit.rs
@@ -0,0 +1,77 @@
+use plonky2::{
+    hash::{
+        hash_types::HashOutTarget,
+        poseidon::PoseidonHash,
+    },
+    iop::target::Target,
+    plonk::circuit_builder::CircuitBuilder,
+};
+
+use crate::types::{F,D};
+
+/// A node in the merkle sum tree, contains the total amount of equity (in usd) and the total amount of debt (in usd) and the hash.
+/// 
+/// The hash is Hash(hash_left, hash_right).
+/// 
+/// The amount of equity and amount of debt is the sum of the equity and debt of the children.
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub struct MerkleSumNodeTarget{
+    pub amount_equity: Target,
+    pub amount_debt: Target,
+    pub hash: HashOutTarget
+}
+
+impl MerkleSumNodeTarget {
+    /// Given two children, generate the next MerkleSumNode
+    pub fn get_child_from_parents(
+        builder: &mut CircuitBuilder<F, D>,
+        left_node: &MerkleSumNodeTarget,
+        right_node: &MerkleSumNodeTarget,
+    )->MerkleSumNodeTarget{
+        let amount_equity = builder.add(left_node.amount_equity, right_node.amount_equity);
+        let amount_debt = builder.add(left_node.amount_debt, right_node.amount_debt);
+
+        let hash_inputs = vec![
+            left_node.hash.elements.to_vec(),
+            right_node.hash.elements.to_vec(),
+        ].concat();
+
+        let hash = builder.hash_n_to_hash_no_pad::<PoseidonHash>(hash_inputs);
+        MerkleSumNodeTarget{
+            amount_equity,
+            amount_debt,
+            hash
+        }
+    }
+}
+
+
+/// We can represent the Merkle Sum Tree as a vector of merkle sum nodes, with the root being the last node in the vector.    
+pub struct MerkleSumTreeTarget{
+    pub sum_tree: Vec<MerkleSumNodeTarget>
+}
+
+impl MerkleSumTreeTarget{
+    /// Gets the root of the merkle sum tree as a Merkle Sum Node
+    pub fn get_root(&self)-> &MerkleSumNodeTarget{
+        self.sum_tree.last().unwrap()
+    }
+}
+
+/// Builds a merkle sum tree of a given size (based on the number of leaves). It will build the merkle sum tree on top of the leaves vector
+/// in order to do the task in place. There is no return value as the input leaves vector is mutated.
+pub fn build_merkle_sum_tree(
+    builder: &mut CircuitBuilder<F, D>,
+    leaves: &mut Vec<MerkleSumNodeTarget>,
+) {
+    let num_leaves = leaves.len();
+
+    for i in num_leaves..(num_leaves * 2 - 1) {
+        let left_child_index = 2 * (i - num_leaves);
+        let right_child_index = 2 * (i - num_leaves) + 1;
+        let left_child = leaves.get(left_child_index).unwrap();
+        let right_child = leaves.get(right_child_index).unwrap();
+        leaves.push(MerkleSumNodeTarget::get_child_from_parents(builder, left_child, right_child));
+    }
+}
+
---
+++ crates/zk-por-core/src/circuits/mod.rs
@@ -0,0 +1,4 @@
+pub mod circuit_config;
+pub mod circuit_utils;
+pub mod merkle_sum_circuit;
+pub mod account_circuit;
