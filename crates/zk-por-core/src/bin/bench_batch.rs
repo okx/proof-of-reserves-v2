5425615a89fec06ec6622a09445e481a2c8357ec
Ruan Pingcheng <pingcheng.ruan@okg.com>
2024-08-02 17:15:35 +0800

    format

--- crates/zk-por-core/src/bin/bench_batch.rs
+++ crates/zk-por-core/src/bin/bench_batch.rs
@@ -6,15 +6,15 @@ use zk_por_core::{
 };
 
 fn main() {
-	let num_accounts = 1024; // configure this for bench.
+    let num_accounts = 1024; // configure this for bench.
 
-	let num_assets = 5;
-	let (circuit_data, account_targets) = build_merkle_sum_tree_circuit(num_accounts, num_assets);
+    let num_assets = 5;
+    let (circuit_data, account_targets) = build_merkle_sum_tree_circuit(num_accounts, num_assets);
 
-	let (accounts, _, _) = gen_accounts_with_random_data(num_accounts, num_assets);
-	let start = std::time::Instant::now();
-	let prover = MerkleSumTreeProver { accounts };
-	_ = prover.prove_with_circuit(&circuit_data, account_targets);
+    let (accounts, _, _) = gen_accounts_with_random_data(num_accounts, num_assets);
+    let start = std::time::Instant::now();
+    let prover = MerkleSumTreeProver { accounts };
+    _ = prover.prove_with_circuit(&circuit_data, account_targets);
 
-	println!("prove {} accounts in batch in : {:?}", num_accounts, start.elapsed());
+    println!("prove {} accounts in batch in : {:?}", num_accounts, start.elapsed());
 }
