76ef6215bc297110cc8e7daf5a3894982161fa38
Ruan Pingcheng <pingcheng.ruan@okg.com>
2024-08-06 15:42:05 +0800

    finish main prover

--- crates/zk-por-core/src/circuit_config.rs
+++ crates/zk-por-core/src/circuit_config.rs
@@ -43,3 +43,21 @@ pub const HIGH_RATE_CONFIG: CircuitConfig = CircuitConfig {
 pub const RECURSIVE_FACTOR: usize = 64;
 pub const BATCH_SIZE: usize = 1024;
 pub const ASSET_NUM: usize = 4;
+
+pub const STANDARD_ZK_CONFIG: CircuitConfig = CircuitConfig {
+    num_wires: 135,
+    num_routed_wires: 80,
+    num_constants: 2,
+    use_base_arithmetic_gate: true,
+    security_bits: 100,
+    num_challenges: 2,
+    zero_knowledge: true,
+    max_quotient_degree_factor: 8,
+    fri_config: FriConfig {
+        rate_bits: 3,
+        cap_height: 1,
+        proof_of_work_bits: 16,
+        reduction_strategy: FriReductionStrategy::ConstantArityBits(4, 5),
+        num_query_rounds: 28,
+    },
+};
