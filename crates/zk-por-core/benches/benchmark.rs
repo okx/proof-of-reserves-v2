9b0e2ad197bf06537adaad4cd524ad4cba9456d3
Ruan Pingcheng <pingcheng.ruan@okg.com>
2024-08-07 17:12:56 +0800

    format

--- crates/zk-por-core/benches/benchmark.rs
+++ crates/zk-por-core/benches/benchmark.rs
@@ -1,40 +1,45 @@
-use criterion::{black_box, criterion_group, criterion_main, BenchmarkGroup, Criterion, measurement::WallTime, SamplingMode};
+use criterion::{
+    black_box, criterion_group, criterion_main, measurement::WallTime, BenchmarkGroup, Criterion,
+    SamplingMode,
+};
 use zk_por_core::{
-	circuit_config::STANDARD_CONFIG,
-	account::gen_accounts_with_random_data,
-	recursive_prover::prover::RecursiveProver,
-	recursive_prover::recursive_circuit::build_recursive_n_circuit,
-	merkle_sum_prover::prover::MerkleSumTreeProver,
-	merkle_sum_prover::circuits::merkle_sum_circuit::build_merkle_sum_tree_circuit,
-	types::{C, D, F},
+    account::gen_accounts_with_random_data,
+    circuit_config::STANDARD_CONFIG,
+    merkle_sum_prover::{
+        circuits::merkle_sum_circuit::build_merkle_sum_tree_circuit, prover::MerkleSumTreeProver,
+    },
+    recursive_prover::{prover::RecursiveProver, recursive_circuit::build_recursive_n_circuit},
+    types::{C, D, F},
 };
 
 use plonky2::plonk::proof::ProofWithPublicInputs;
 
 pub fn bench_batch_circuit(c: &mut BenchmarkGroup<WallTime>, batch_size: usize) {
-	let num_assets = 200;
+    let num_assets = 200;
     let (circuit_data, account_targets) =
         build_merkle_sum_tree_circuit(batch_size, num_assets, STANDARD_CONFIG);
     let accounts = gen_accounts_with_random_data(batch_size, num_assets);
 
-    c.bench_function(format!("batch_circuit_{}", batch_size).as_str(), |b| b.iter( || {
-		let prover = MerkleSumTreeProver { accounts: accounts.clone() };
-		_ = prover.get_proof_with_circuit_data(account_targets.clone(), &circuit_data);
-	}));
+    c.bench_function(format!("batch_circuit_{}", batch_size).as_str(), |b| {
+        b.iter(|| {
+            let prover = MerkleSumTreeProver { accounts: accounts.clone() };
+            _ = prover.get_proof_with_circuit_data(account_targets.clone(), &circuit_data);
+        })
+    });
 }
 
-pub fn batch_circuit(c : &mut Criterion) {
-	let mut group = c.benchmark_group("batch_circuit");
-	group.sample_size(10);
-	bench_batch_circuit(&mut group, 16);
-	bench_batch_circuit(&mut group, 64);
-	bench_batch_circuit(&mut group, 256);
-	bench_batch_circuit(&mut group, 512);
-	bench_batch_circuit(&mut group, 1024);
-	group.finish();
+pub fn batch_circuit(c: &mut Criterion) {
+    let mut group = c.benchmark_group("batch_circuit");
+    group.sample_size(10);
+    bench_batch_circuit(&mut group, 16);
+    bench_batch_circuit(&mut group, 64);
+    bench_batch_circuit(&mut group, 256);
+    bench_batch_circuit(&mut group, 512);
+    bench_batch_circuit(&mut group, 1024);
+    group.finish();
 }
 
-pub fn bench_recursive_circuit<const SUBPROOF_NUM : usize>(g: &mut BenchmarkGroup<WallTime>) {
+pub fn bench_recursive_circuit<const SUBPROOF_NUM: usize>(g: &mut BenchmarkGroup<WallTime>) {
     let batch_size = 1024;
     let asset_num = 4;
     let (merkle_sum_circuit, account_targets) =
@@ -54,25 +59,28 @@ pub fn bench_recursive_circuit<const SUBPROOF_NUM : usize>(g: &mut BenchmarkGrou
     let subproofs: [ProofWithPublicInputs<F, C, D>; SUBPROOF_NUM] =
         std::array::from_fn(|_| merkle_sum_proof.clone());
 
-	g.bench_function(format!("recursive_circuit_{}", SUBPROOF_NUM).as_str(), |b| b.iter( || {
-		let recursive_prover = RecursiveProver {
-			sub_proofs: subproofs.clone(),
-			sub_circuit_vd: merkle_sum_circuit.verifier_only.clone(),
-		};
-		recursive_prover.get_proof_with_circuit_data(recursive_targets.clone(), &recursive_circuit);
-	}));
+    g.bench_function(format!("recursive_circuit_{}", SUBPROOF_NUM).as_str(), |b| {
+        b.iter(|| {
+            let recursive_prover = RecursiveProver {
+                sub_proofs: subproofs.clone(),
+                sub_circuit_vd: merkle_sum_circuit.verifier_only.clone(),
+            };
+            recursive_prover
+                .get_proof_with_circuit_data(recursive_targets.clone(), &recursive_circuit);
+        })
+    });
 }
 
-pub fn recursive_circuit(c : &mut Criterion) {
-	let mut group = c.benchmark_group("recursive_circuit");
-	group.sample_size(10);
-	group.sampling_mode(SamplingMode::Flat); // for long running benchmarks
-	bench_recursive_circuit::<4>(&mut group);
-	bench_recursive_circuit::<8>(&mut group);
-	bench_recursive_circuit::<16>(&mut group);
-	bench_recursive_circuit::<32>(&mut group);
-	bench_recursive_circuit::<64>(&mut group);
-	group.finish();
+pub fn recursive_circuit(c: &mut Criterion) {
+    let mut group = c.benchmark_group("recursive_circuit");
+    group.sample_size(10);
+    group.sampling_mode(SamplingMode::Flat); // for long running benchmarks
+    bench_recursive_circuit::<4>(&mut group);
+    bench_recursive_circuit::<8>(&mut group);
+    bench_recursive_circuit::<16>(&mut group);
+    bench_recursive_circuit::<32>(&mut group);
+    bench_recursive_circuit::<64>(&mut group);
+    group.finish();
 }
 
 criterion_group!(benches, batch_circuit, recursive_circuit);
--- crates/zk-por-core/src/bin/bench_prover.rs
+++ crates/zk-por-core/src/bin/bench_prover.rs
@@ -1,5 +1,7 @@
 use zk_por_core::{
-    account::{gen_accounts_with_random_data, Account}, circuit_registry::registry::CircuitRegistry, config::ProverConfig,
+    account::{gen_accounts_with_random_data, Account},
+    circuit_registry::registry::CircuitRegistry,
+    config::ProverConfig,
 };
 
 use plonky2::{hash::hash_types::HashOut, plonk::proof::ProofWithPublicInputs};
@@ -19,21 +21,23 @@ struct Proof {
     proof: ProofWithPublicInputs<F, C, D>,
 }
 
-/// this binary to benchmark e2e proving. 
-/// we donot use criterion because it will run the benchmark multiple times, which will be too slow. 
+/// this binary to benchmark e2e proving.
+/// we donot use criterion because it will run the benchmark multiple times, which will be too slow.
 fn main() {
-	let args: Vec<String> = env::args().collect();
-    let file_num: usize = args.get(1)
+    let args: Vec<String> = env::args().collect();
+    let file_num: usize = args
+        .get(1)
         .expect("Please provide the file number as the first argument")
         .parse()
         .expect("The provided file number must be a valid usize");
 
-    let per_file_batch_num: usize = args.get(2)
+    let per_file_batch_num: usize = args
+        .get(2)
         .expect("Please provide the per file batch number as the second argument")
         .parse()
         .expect("The provided number must be a valid usize");
 
-	let cfg = TraceConfig {
+    let cfg = TraceConfig {
         prefix: "zkpor-bench".to_string(),
         dir: "logs".to_string(),
         level: tracing::Level::INFO,
@@ -62,17 +66,22 @@ fn main() {
     );
 
     let (tx, rx) = mpsc::channel::<Vec<Account>>();
-	let start = Instant::now();
+    let start = Instant::now();
     let prover =
         thread::spawn(move || stream_prove(rx, &circuit_registry, batch_size, PROVING_THREADS_NUM));
 
     thread::spawn(move || {
-		(0..file_num).for_each(|_| {
-            let accounts = gen_accounts_with_random_data(per_file_batch_num * batch_size, asset_num);
+        (0..file_num).for_each(|_| {
+            let accounts =
+                gen_accounts_with_random_data(per_file_batch_num * batch_size, asset_num);
             tx.send(accounts).unwrap()
-		})
+        })
     });
 
     _ = prover.join().unwrap();
-	tracing::info!("finish e2e proving {} accounts in {:?}", batch_size * file_num * per_file_batch_num, start.elapsed());
+    tracing::info!(
+        "finish e2e proving {} accounts in {:?}",
+        batch_size * file_num * per_file_batch_num,
+        start.elapsed()
+    );
 }
