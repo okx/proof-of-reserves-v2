e5ab50224c4d42b2e1b7d19a21aeb5192f08de71
cliff0412 <cliff.yang@okg.com>
2024-08-13 13:53:25 +0800

    fmt

--- crates/zk-por-core/src/e2e.rs
+++ crates/zk-por-core/src/e2e.rs
@@ -26,8 +26,8 @@ pub fn batch_prove_accounts<const RECURSION_BRANCHOUT_NUM: usize>(
                 .par_chunks(batch_size)
                 .map(|accounts| {
                     let prover = MerkleSumTreeProver { accounts: accounts.to_owned() };
-                    let proof =
-                        prover.get_proof_with_circuit_data(account_targets.to_vec(), &batch_circuit);
+                    let proof = prover
+                        .get_proof_with_circuit_data(account_targets.to_vec(), &batch_circuit);
                     proof
                 })
                 .collect();
