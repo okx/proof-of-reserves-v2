522edb9e6d09e542ea586948a539a99427c8f1ce
cliff0412 <cliff.yang@okg.com>
2024-08-19 14:35:36 +0800

    fmt

--- crates/zk-por-cli/src/verifier.rs
+++ crates/zk-por-cli/src/verifier.rs
@@ -77,8 +77,7 @@ pub fn verify(
         let mut _g = global_mst.read().expect("unable to get a lock");
         let gmst_root = _g.inner.last().unwrap();
 
-        let _ =
-            proof.verify_merkle_proof(&account, *gmst_root).expect("Invalid Merkle Proof");
+        let _ = proof.verify_merkle_proof(&account, *gmst_root).expect("Invalid Merkle Proof");
 
         println!("successfully verify the inclusion proof for user for round {}", round_num);
     }
