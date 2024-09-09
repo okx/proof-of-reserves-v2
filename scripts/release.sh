79308030428833666295b0fcd7c1bca6202052a4
Ruan Pingcheng <pingcheng.ruan@okg.com>
2024-09-09 13:26:07 +0800

    update sed cmd for linux

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,8 +7,7 @@ cargo build --features zk-por-core/verifier --release
 mv target/release/zk-por-cli release/zk-por-verifier
 
 mkdir -p release/config
-cp config/default.toml release/config/default.toml
-sed -i '' 's|/opt/data/zkpor/users/|./sample_data/|g' release/config/default.toml
+sed 's|/opt/data/zkpor/users/|./sample_data/|g' config/default.toml > release/config/default.toml
 
 mkdir -p release/sample_data
 cp -r test-data/batch0.json release/sample_data
